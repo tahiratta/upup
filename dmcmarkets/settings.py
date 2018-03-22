66a0f2bead8fb3837eb942a652cb5d9d90cd8d21
Tahir <tahiratta27@gmail.com>
2018-03-22 06:00:44 -0700

    let do

--- dmcmarkets/settings.py
+++ dmcmarkets/settings.py
@@ -27,8 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.100.8', 'localhost', '127.0.0.1', 'dmcmarket.herokuapp.com']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Application definition
@@ -135,5 +136,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media/')
-# MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media/')
+MEDIA_URL = '/media/'
